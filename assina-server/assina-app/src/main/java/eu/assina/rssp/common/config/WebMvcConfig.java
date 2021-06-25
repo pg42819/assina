aa4e87a8f97976029d4fced7b4148194ae43c300
cwerner <chriswerner@fico.com>
2021-06-25 22:21:20 +0100

    fixed cors

--- assina-server/assina-app/src/main/java/eu/assina/rssp/common/config/WebMvcConfig.java
+++ assina-server/assina-app/src/main/java/eu/assina/rssp/common/config/WebMvcConfig.java
@@ -1,6 +1,8 @@
 package eu.assina.rssp.common.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.method.HandlerTypePredicate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -12,11 +14,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     private final long MAX_AGE_SECS = 3600;
 
+    @Autowired
+    public Environment env;
+
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        String clientUrl = env.getProperty("ASSINA_CLIENT_BASE_URL");
         registry.addMapping("/**")
-                // TODO generalize localhost for client
-        .allowedOrigins("http://localhost:3000")
+        .allowedOrigins(clientUrl)
 //            .allowedOrigins("*")
         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
         .allowedHeaders("*")
--- assina-server/assina-sa/src/main/java/eu/assina/sa/config/WebMvcConfig.java
+++ assina-server/assina-sa/src/main/java/eu/assina/sa/config/WebMvcConfig.java
@@ -1,28 +1,36 @@
 package eu.assina.sa.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.method.HandlerTypePredicate;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    private static final Logger log = LoggerFactory.getLogger(WebMvcConfig.class);
     private final long MAX_AGE_SECS = 3600;
+
+    @Autowired
+    public Environment env;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        String clientUrl = env.getProperty("ASSINA_CLIENT_BASE_URL");
+        log.warn("CORS: Allowing client origin: {}", clientUrl);
         registry.addMapping("/**")
                 // TODO generalize localhost for client
-        .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(clientUrl)
 //            .allowedOrigins("*")
-        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-        .allowedHeaders("*")
-        .allowCredentials(true)
-        .maxAge(MAX_AGE_SECS);
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(MAX_AGE_SECS);
     }
+
 //
 //    @Override
 //    public void configurePathMatch(PathMatchConfigurer configurer) {
