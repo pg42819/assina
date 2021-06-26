5b10ab465c860b5c8f5f44a3a279b772da8fc4e5
cwerner <chriswerner@fico.com>
2021-06-26 12:19:56 +0100

    udpated start client script

--- tools/client_scripts/start_client.sh
+++ tools/client_scripts/start_client.sh
@@ -1,4 +1,7 @@
 cd /home/franciscorosa/assina
+export REACT_APP_ASSINA_RSSP_BASE_URL=http://13.93.7.40.xip.io:80
+export REACT_APP_ASSINA_SA_BASE_URL=http://20.101.144.136:8081
+export REACT_APP_ASSINA_CLIENT_BASE_URL=http://localhost
 nohup sudo npm start &
 #echo $#
 #node node_modules/react-scripts/scripts/start.js
